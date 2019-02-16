6ac2704388ca842f52defc6f9193aa2824cf94fa
Tai <thm6@njit.edu>
2019-02-16 02:31:49 -0500

    Feature: Using a factory (in Progress)

--- public/index.php
+++ public/index.php
@@ -5,4 +5,83 @@
  * Date: 2/15/19
  * Time: 11:02 PM
  */
-echo 'Hello IS601 2019';
+
+main::start();
+
+class main {
+
+    static public function start() {
+       $records = csv::getRecords();
+       $table = html::generateTable($records);
+
+       system::printPage($table);
+
+    }
+
+}
+
+class csv {
+    static public function getRecords() {
+
+        $make = 'ford';
+        $model = 'Taurus';
+        $car=AutomobileFactory::create($make, $model);
+        print_r($car);
+        $records[] = $car;
+
+        print_r($records);
+
+
+        return $records;
+
+    }
+
+
+}
+
+class html {
+
+    static public function generateTable($records) {
+
+        $table = $records;
+
+        return $table;
+    }
+
+}
+
+class system {
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+
+
+}
+
+
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
