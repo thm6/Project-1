727fa2492c4689e15768ddde40ab661fb6432ade
Tai <thm6@njit.edu>
2019-02-16 23:00:58 -0500

    Feature: Adding csv file

--- public/index.php
+++ public/index.php
@@ -14,9 +14,7 @@ class main
     static public function start($filename)
     {
         $records = csv::getRecords($filename);
-        $table = html::generateTable($records);
 
-        print_r($records);
 
     }
 }
@@ -27,23 +25,63 @@ class csv{
 
         $file = fopen($filename, "r");
 
+        $fieldNames = array();
+
+        $count = 0;
+
         while (!feof($file))
         {
             $record[] = fgetcsv($file);
+            if($count == 0){
+                $fieldNames = $record;
+            }
+            else{
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
 
-            $records[] = $record;
+            $count++;
+
         }
 
         fclose($file);
         return $records;
-        print_r($records);
-
-
-
-
-
 
     }
 
+}
+
+class record {
+
+    public function __construct(Array $fieldNames = null, $values = null)
+    {
+        print_r($fieldNames);
+        print_r($values);
+        $record = array_combine($fieldNames, $values);
+        print_r($record);
+
+
+        $this->createProperty();
+
+    }
+
+    public function createProperty($name = "first", $value = 'keith') {
+
+        $this->{$name} = $value;
+
+    }
+
+
+}
+class recordFactory{
+
+    public static function create(Array $fieldNames = null, Array $values = null) {
+
+
+        $record = new record($fieldNames, $values);
+
+        return $record;
+
+
+    }
 
 }
