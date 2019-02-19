d49f785468d69bd8f51713da3dd2bf1b32dc371e
Tai <thm6@njit.edu>
2019-02-19 01:39:44 -0500

    Fix: updated class csv contents

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 /**
  * Created by PhpStorm.
  */
+
 $csvTable = new csvTable;
 $csvTable->start('file.csv');
 
@@ -45,42 +46,39 @@ class csvTable
         return $table;
     }
     protected function outputFooter() {
-        print "\n".'<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>'
-            .'<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>'
-            .'<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>';
+        print "\n".'<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" 
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>'
+            .'<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" 
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>'
+            .'<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" 
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>';
 
         print '</body></html>';
-
 }
 
 
-
 }
-
 
 class csv{
 
     static public function getRecords($filename){
 
-        $file = fopen($filename,"r");
-
-        $fieldNames = array();
+        $file = fopen($filename, "r");
 
         $count = 0;
-
         $tableRecords = array();
-
-        while (!feof($file)) {
-
+        while(! feof($file))
+        {
             $record = fgetcsv($file);
             $tableRecords[] = $record;
-
             $count++;
         }
+        /**
+         * I encountered unexplainable stopping of code execution in class RecordFactory,
+         * so rewrote some code and refactored a few times.
+         **/
         return $tableRecords;
         fclose($file);
-        print '<br>fieldNames is ';
-        print_r($fieldNames);
         return $records;
 
     }
