6201abf00f9e97f7e0191a3e9ae3485c3f958e00
Tai <thm6@njit.edu>
2019-02-19 01:26:56 -0500

    Task: Added Protected function

--- public/index.php
+++ public/index.php
@@ -14,7 +14,6 @@ class csvTable
         $records = csv::getRecords($filename);
         $tableHTML = $this->createTable($records);
         $docHead = '<!doctype html><html lang="en"><head>'
-//            .'rel="stylesheet" type="text/css" href="style.css">'
             . '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
                 integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">'
             . '</head><body>';
@@ -35,7 +34,7 @@ class csvTable
             } else $tag = 'td';
             $table .= "\n<tr>";
             foreach ($row as $kk => $cell) {
-                // sanitize our output from file
+
                 $table .= "\n<" . $tag . '>' . htmlspecialchars($cell) . '</' . $tag . '>';
             }
             $table .= "\n</tr>";
@@ -45,19 +44,17 @@ class csvTable
         $table .= '</tbody></table>';
         return $table;
     }
+    protected function outputFooter() {
+        print "\n".'<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>'
+            .'<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>'
+            .'<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>';
 
-
-
-
-
-
+        print '</body></html>';
 
 }
 
 
 
-
-    }
 }
 
 
